324a4177b0314c70d76c22a5e8f25a9118ad5744
wattai <evamark.wattai@gmail.com>
2024-11-20 22:48:10 +0900

    Add 003-chat-with-pdf-vision

---
+++ scripts/003-chat-with-pdf-vision.sh
@@ -0,0 +1,24 @@
+modal run scripts/003-chat-with-pdf-vision.py
+
+# ❯ ./scripts/003-chat-with-pdf-vision.sh
+# ✓ Initialized. View run at https://modal.com/apps/wattai/main/ap-XXX
+# ✓ Created objects.
+# ├── 🔨 Created mount /home/wattai/dev/pg-modal/scripts/003-chat-with-pdf-vision.py
+# ├── 🔨 Created function convert_pdf_to_images.
+# ├── 🔨 Created function download_model.
+# ├── 🔨 Created function Model.load_models.
+# ├── 🔨 Created function Model.*.
+# ├── 🔨 Created function Model.index_pdf.
+# └── 🔨 Created function Model.respond_to_message.
+# Starting a new session with id f13178c3-06f5-4e9c-85e6-bad75707515f
+# Indexing PDF from https://arxiv.org/pdf/1706.03762
+# `Qwen2VLRotaryEmbedding` can now be fully parameterized by passing the model config through the `config` argument. All other arguments will be removed in v4.46
+# Loading checkpoint shards: 100%|██████████| 2/2 [00:06<00:00,  3.08s/it]
+# Loading checkpoint shards: 100%|██████████| 2/2 [00:01<00:00,  1.07it/s]
+# QUESTION: What is this document about?
+# This document is about the Transformer model, a type of neural network architecture that has been proposed as a solution to the challenge of 
+# sequence transduction tasks. The Transformer model is designed to be simple, efficient, and parallelizable, and it has shown superior 
+# performance compared to other models on various tasks.
+# Stopping app - local entrypoint completed.
+# Runner terminated.
+# ✓ App completed. View run at https://modal.com/apps/wattai/main/ap-XXX
